2d7be5bd4c4a90c1d2ebe74e441500233fee481e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-02-26 20:31:37 -0700

    Updated the location of the private plugins

--- keymando/plugins/05_supplemental.rb
+++ keymando/plugins/05_supplemental.rb
@@ -1,3 +1,3 @@
-supplemental_folder = "#{ENV['HOME']}/repositories/developwithpassion/devtools/mac/keymando"
+private_folder = "#{ENV['HOME']}/repositories/developwithpassion/keymando_private_plugins/*.rb"
 Dir.glob(File.join(supplemental_folder,"**/*.rb")).each{|file| load(File.expand_path(file))} if Dir.exist?(supplemental_folder)
 
